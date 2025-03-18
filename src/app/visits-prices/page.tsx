d71d0fed8208f8b748d668e41817e9ccd20d41d0
OscarAntayhuaCastillo <u20191e414@upc.edu.pe>
2025-03-18 15:00:51 -0500

    fixed

--- src/app/visits-prices/page.tsx
+++ src/app/visits-prices/page.tsx
@@ -1,12 +1,14 @@
-import CardsUser from "@/components/ui/cards-user";
-
+import GrillaCard from "@/components/ui/grilla-card";
 export default function VisitsPage() {
   return (
     <div className="container mx-auto py-10">
+      {/* 🔹 Título separado */}
       <h1 className="text-4xl font-bold text-center text-green-800 mb-8">
         Tutti i Tipi di Visita
       </h1>
-      <CardsUser columns={3} />
+
+      {/* 🔹 La grilla de tarjetas */}
+      <GrillaCard />
     </div>
   );
 }
