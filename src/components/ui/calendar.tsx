bb264182e12d4d04bbcad1ba320a28f631481238
ChumanY <cuerpoespin_2000@hotmail.com>
2025-03-18 11:07:43 -0500

    feat: refactor calendar part 3

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -21,18 +21,18 @@ function Calendar({
       className={cn("p-3", className)}
       classNames={{
         month: "space-y-4",
-        months: "flex flex-col sm:flex-row space-y-4 sm:space-y-0 relative",
+        months: "flex flex-col sm:flex-row gap-4 sm:gap-8 relative",
         month_caption: "flex justify-center pt-1 relative items-center",
         month_grid: "w-full border-collapse space-y-1",
         caption_label: "text-sm font-medium",
         nav: "flex items-center justify-between absolute inset-x-0",
         button_previous: cn(
           buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 z-10",
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 z-10"
         ),
         button_next: cn(
           buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 z-10",
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 z-10"
         ),
         weeks: "w-full border-collapse space-y-",
         weekdays: "flex",
@@ -43,7 +43,7 @@ function Calendar({
           "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-9 w-9 p-0 font-normal aria-selected:opacity-100",
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
         ),
         range_end: "day-range-end",
         selected:
